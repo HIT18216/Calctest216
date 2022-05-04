e5c26014e89d05affcde9cbfbb7cfcb60813d0a6
HIT18216 <100738514+HIT18216@users.noreply.github.com>
2022-05-04 09:27:15 +0900

    Create setup.py

---
+++ setup.py
@@ -0,0 +1,18 @@
+import setuptools
+
+setuptools.setup(
+  include_package_data = True,
+  name = 'Calctest216'
+  version = '1.0.0'
+  description = 'oss-dev my claculator',
+  author = 'hwKim',
+  author_email = '1828989@donga.ac.kr',
+  url = 'https://github.com/HIT18216/Calctest216',
+  download_url = 'https://github.com/HIT18216/Calctest216/blob/main/mycalculator/calculator.py'
+  install_requires = ['pytest'],
+  long-description_content_type = 'text/markdown'
+  classifiers = [
+    "Programming Language :: Python :: 3",
+    "Operating System :: OS Independent",
+  ],
+)
